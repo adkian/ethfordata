f203b5e8b10282f41f5c4dd6dc890c66cf939ea2
Aditya Naik <naik@psu.edu>
2017-10-15 04:39:24 -0400

    post debugging pre testrpc deployment

--- master.sol
+++ master.sol
@@ -6,7 +6,7 @@ contract ClientInterface {
   
   struct Datapoint{
     uint client_id;
-    int timestamp;
+    uint timestamp;
     int lat;
     int long;
   }
@@ -25,24 +25,31 @@ contract ClientInterface {
   /* called by the client when sending location data */
   function getData(uint ID, uint time, int lat, int long) external returns (bool){
     uint dp = numDatapoint++;
-    end_data_point++;
-    datapoints[dp] = Datapoint[ID, time, lat, long];
+    end_data_index++;
+    datapoints[dp] = Datapoint(ID, time, lat, long);
   }
 
   mapping (uint => address) clients;
   /* called by a new client during initialization */
   /* returns a unique ID to the client which is the identifier  */
   /* used to identify client during incoming data transactions */
- /* Roadmap: hashed unique ID */
+  /* Roadmap: hashed unique ID */
   function getID() external returns (uint){
     clients[numClients] = msg.sender;
     return numClients++;
   }
 }
 
+contract FunderInterface{
+  function sendData(string data){}
+}
 
-contract Master is ClientInterface{
+contract Master is ClientInterface, FunderInterface{
 
+  /* Monthly subscription cost, currently held at 1 ETH */
+  /* Ideally, this rate would be dynamic based on amount of data available  */
+  uint subscription = 1;
+  
   function getBalance() constant returns (uint){
     return this.balance;
   }
@@ -50,63 +57,25 @@ contract Master is ClientInterface{
   /* struct to hold funders data */
   struct Funder{
     address add;
-    uint ID;
+    int ID;
     uint amount;
-    uint tier;    
   }
 
-  uint numFunders;
-  mapping (uint => Funder) funders;
+  int numFunders;
+  mapping (int => Funder) funders;
  
 
   /* funder struct getter functions */
-  function getFunderID(address add) constant returns (uint){
+  function getFunderID(address add) constant returns (int){
     for(int x=0;x<numFunders;x++){
-      if(Funders[x].add == add)
+      if(funders[x].add == add)
 	return x;
     }
     return -1;
   }  
-  function getFunderAmount(uint ID) constant returns (uint){
-    return Funders[ID].amount;
-  }
-  Function getFunderTier(uint ID) constant returns (uint){
-    return Funders[ID].tier;
-  }
-
-  
-  
-  modifier onlyOwner(){
-    if(msg.sender != owner)
-      revert();	
-    else
-      _;
-  }
-
-  /***assign values based on Vlad's results** */
-  /* return a requesting funder's current tier; */
-  /* tier 1 (upper) cutoff: t1cutoff */
-  /* tier 2 (upper) cutoff: t2cutoff */
-  /* tier 3 (upper) cutoff: t3cutoff */
-  function getTier(uint amount) returns (uint){
-    uint t1cutoff;
-    uint t2cutoff;
-    uint t3cutoff;
-
-    uint tier;
-
-    if(amount<t3cutoff){
-      tier = 0;
-    }else if(amount>=t3cutoff && amount<t2cutoff){
-      tier = 3;
-    }else if(amount>=t2cutoff && amount<t1cutoff){
-      tier = 2;
-    }else if(amount>=t1){
-      tier = 1;
-    }
-
-    return tier;
-  }
+  function getFunderAmount(int ID) constant returns (uint){
+    return funders[ID].amount;
+  }  
   
   /* Fund the contract. Should also act as a funder registration function */
   function fund() payable returns (bool){
@@ -117,44 +86,17 @@ contract Master is ClientInterface{
     //if the funder is not in the system, add them
     if(id<0){
       address funderAddress = msg.sender;
-      uint funderID = numFunders++;
+      int funderID = numFunders++;
       uint funderAmount = msg.value;
-      uint funderTier = getTier(funderAmount);
 
       //add all this info into a new struct;   
-      funders[funderID] = Funder(funderAddress, funderID, funderAmount, funderTier);
+      funders[funderID] = Funder(funderAddress, funderID, funderAmount);
     }
     else{
       funders[id].amount+=msg.value;      
     }
         
     return true;      
-  }
-
-  /* function that sends data and payment to relevant parties */
-  function payout() returns (bool){
-    //format data
-    string tx_data;
-    string newlinechar = "\n";
-    for(int d = start_data_index; d<end_data_index; d++){
-      strConcat(txdata, datapoints[d].id, datapoints[d].timestamp, datapoints[d].lat, datapoints[d].long, newlinechar);
-    }
-
-    //TODO: different tiers of data based on granularity
-    //TODO: if payout loop causes errors on the last client due to gas, run a seperate condition for the last client for now
-    /* Send data as a tx data */
-    /* Requires an external function on the client contract to send data to */
-    for(int x=0; x<numFunders; x++){
-      funders[x].address.call(tx_data);
-    }
-
-    //payouts to clients
-    uint share = getBalance()/numClients;
-
-    //may cause errors on the last one: gas costs are not considered yet
-    for(int x=0; x<numClients; x++){
-      clients[x].transfer(share); 
-    }
   }
 
   //TODO: test this chunk of code before testrpc deployment
@@ -165,24 +107,108 @@ contract Master is ClientInterface{
     bytes memory _ba = bytes(user_id);
     bytes memory _bb = bytes(timestamp);
     bytes memory _bc = bytes(lat);
-    bytes memory _bd = bytes(_long);
+    bytes memory _bd = bytes(long);
     bytes memory _newlinechar = bytes(newlinechar);
-    
+     
     string memory formatted_data_point = new string(_base.length +_ba.length + _bb.length + _bc.length + _bd.length + _newlinechar.length);
     
     bytes memory data = bytes(formatted_data_point);
     
     uint k = 0;
     for (uint i = 0; i < _base.length; i++) data[k++] = _base[i];
-    for (uint i = 0; i < _ba.length; i++) data[k++] = _ba[i];
-    for (uint i = 0; i < _bb.length; i++) data[k++] = _bb[i];
-    for (uint i = 0; i < _bc.length; i++) data[k++] = _bc[i];
-    for (uint i = 0; i < _bd.length; i++) data[k++] = _bd[i];
-    for (uint i = 0; i < _newlinechar.length; i++) data[k++] = _newlinechar[i];
+    for (uint i2 = 0; i2 < _ba.length; i2++) data[k++] = _ba[i2];
+    for (uint i3 = 0; i3 < _bb.length; i3++) data[k++] = _bb[i3];
+    for (uint i4 = 0; i4 < _bc.length; i4++) data[k++] = _bc[i4];
+    for (uint i5 = 0; i5 < _bd.length; i5++) data[k++] = _bd[i5];
+    for (uint i6 = 0; i6 < _newlinechar.length; i6++) data[k++] = _newlinechar[i6];
 
     return string(data);
   }
 
+
+  /* function that sends data and payment to relevant parties */
+  function payout() returns (bool){
+    //format data
+    string tx_data;
+    
+    string newlinechar;
+    
+    for(uint d = start_data_index; d<end_data_index; d++){
+        
+      //TODO: convert each datapoint elements to strings
+	strConcat(tx_data, uintToString(datapoints[d].client_id), uintToString(datapoints[d].timestamp), getCoordString(datapoints[d].lat), getCoordString(datapoints[d].long), "\n");
+    }
+    
+    //TODO: Different tiers of data based on granularity
+    //TODO: if payout loop causes errors on the last client due to gas, run a seperate condition for the last client for now
+    /* Send data as a tx data */
+    /* Requires an external function on the client contract to send data to */    
+    uint totalcharge=0;
+    for(int x=0; x<numFunders; x++){
+      if(funders[x].amount>=subscription){
+        FunderInterface f = FunderInterface(funders[x].add);
+        f.sendData(tx_data);
+	//funders[x].call(tx_data);
+	
+	funders[x].amount -= subscription;
+	totalcharge += subscription;
+      }	      
+    }
+    
+    //payouts to clients
+    uint share = totalcharge/numClients;
+
+    //may cause errors on the last one: gas costs are not considered yet
+    for(uint curr_client=0; curr_client<numClients; curr_client++){
+      clients[curr_client].transfer(share); 
+    }
+  }
+  
+  function uintToString(uint v) constant returns (string str) {
+    uint maxlength = 100;
+    bytes memory reversed = new bytes(maxlength);
+    uint i = 0;
+    while (v != 0) {
+      uint remainder = v % 10;
+      v = v / 10;
+      reversed[i++] = byte(48 + remainder);
+    }
+    bytes memory s = new bytes(i + 1);
+    for (uint j = 0; j <= i; j++) {
+      s[j] = reversed[i - j];
+    }
+    str = string(s);
+  }
+
+  /* ... */
+  function getCoordString(int coord) returns (string){
+    string neg;
+    if(coord<0){
+      coord*=-1;
+      return getNegCoordString("-", uintToString(uint(coord)));   
+    }
+    else{
+      return uintToString(uint(coord));
+    }
+  }
+  
+  /* O_O */
+  function getNegCoordString(string base, string user_id) internal returns (string){
+    
+    bytes memory _base = bytes(base);
+    bytes memory _ba = bytes(user_id);
+
+     
+    string memory formatted_data_point = new string(_base.length +_ba.length);
+    
+    bytes memory data = bytes(formatted_data_point);
+    
+    uint k = 0;
+    for (uint i = 0; i < _base.length; i++) data[k++] = _base[i];
+    for (uint i2 = 0; i2 < _ba.length; i2++) data[k++] = _ba[i2];
+
+    return string(data);
+  }
 }
 
 
